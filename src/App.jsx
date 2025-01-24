d4be1f9917311b0ae360d91d26dfcce0c949d320
KetanMediaamp <ketan.kedar@mediaamp.co.in>
2025-01-24 15:45:57 +0530

    experiment thingy

--- src/App.jsx
+++ src/App.jsx
@@ -1,186 +1,108 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "@progress/kendo-theme-default/dist/all.css";
-import {
-  Chart,
-  ChartSeries,
-  ChartSeriesItem,
-  ChartCategoryAxis,
-  ChartCategoryAxisItem,
-  ChartTitle,
-  ChartLegend,
-} from "@progress/kendo-react-charts";
-const categories = [2002, 2003, 2004];
-const series = [
+import "bootstrap/dist/css/bootstrap.min.css";
+// import * as React from "react";
+
+// import { data } from "./ch-weather-data.js";
+import { Col, Container, Row } from "react-bootstrap";
+
+// console.log(data);
+const data = [
   {
-    name: "India",
-    data: [3.907, 7.943, 7.848],
+    description: "Lady with a Teddy",
+    "image-url":
+      "https://images.pexels.com/photos/29937530/pexels-photo-29937530/free-photo-of-stunning-northern-lights-over-british-columbia.jpeg",
   },
   {
-    name: "Russian Federation",
-    data: [4.743, 7.295, 7.175],
+    description: "Girl with camera",
+    "image-url":
+      "https://images.pexels.com/photos/30254787/pexels-photo-30254787/free-photo-of-cozy-coffee-setup-with-plants-and-books.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    name: "Germany",
-    data: [0.21, 0.375, 1.161],
+    description: "Beautiful Girl with Glasses",
+    "image-url":
+      "https://images.pexels.com/photos/30305131/pexels-photo-30305131/free-photo-of-colorful-hindu-temple-at-batu-caves-malaysia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    name: "World",
-    data: [1.988, 2.733, 3.994],
+    description: "Redhead with frackles",
+    "image-url":
+      "https://images.pexels.com/photos/16762023/pexels-photo-16762023/free-photo-of-village-with-church-in-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    description: "Girl in black dress",
+    "image-url":
+      "https://images.pexels.com/photos/9943963/pexels-photo-9943963.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    description: "Girl Sitting on Chair",
+    "image-url":
+      "https://images.pexels.com/photos/6691721/pexels-photo-6691721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    description: "Girl Sitting on Chair",
+    "image-url":
+      "https://images.pexels.com/photos/6691721/pexels-photo-6691721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    description: "Girl Sitting on Chair",
+    "image-url":
+      "https://images.pexels.com/photos/6691721/pexels-photo-6691721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    description: "Girl Sitting on Chair",
+    "image-url":
+      "https://images.pexels.com/photos/6691721/pexels-photo-6691721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    description: "Girl Sitting on Chair",
+    "image-url":
+      "https://images.pexels.com/photos/6691721/pexels-photo-6691721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    description: "Girl Sitting on Chair",
+    "image-url":
+      "https://images.pexels.com/photos/6691721/pexels-photo-6691721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
 ];
-const areaData = [
-  {
-    name: "World",
-    data: [3.988, 3.733, 3.994],
-  },
-  {
-    name: "Germany",
-    data: [2.21, 2.375, 2.161],
-  },
-  {
-    name: "Russian Federation",
-    data: [1.743, 1.295, 1.175],
-  },
-  {
-    name: "India",
-    data: [0.907, 0.943, 0.848],
-  },
-];
-const pieData = [
-  {
-    name: "India",
-    share: 0.24,
-  },
-  {
-    name: "Russian Federation",
-    share: 0.26,
-    explode: true,
-  },
-  {
-    name: "Germany",
-    share: 0.1,
-  },
-  {
-    name: "World",
-    share: 0.4,
-  },
-];
-const ChartContainer = () => (
-  <>
-    <div className="row mb-3">
-      <div className="col-6">
-        <div className="k-card">
-          <Chart
-            style={{
-              height: 350,
-            }}
-          >
-            <ChartTitle text="Column Chart" />
-            <ChartLegend position="top" orientation="horizontal" />
-            <ChartCategoryAxis>
-              <ChartCategoryAxisItem categories={categories} startAngle={45} />
-            </ChartCategoryAxis>
-            <ChartSeries>
-              {series.map((item, idx) => (
-                <ChartSeriesItem
-                  key={idx}
-                  type="column"
-                  tooltip={{
-                    visible: true,
-                  }}
-                  data={item.data}
-                  name={item.name}
-                />
-              ))}
-            </ChartSeries>
-          </Chart>
-        </div>
-      </div>
-      <div className="col-6">
-        <div className="k-card">
-          <Chart
-            style={{
-              height: 350,
-            }}
-          >
-            <ChartTitle text="Line Chart" />
-            <ChartLegend position="top" orientation="horizontal" />
-            <ChartCategoryAxis>
-              <ChartCategoryAxisItem categories={categories} startAngle={45} />
-            </ChartCategoryAxis>
-            <ChartSeries>
-              {series.map((item, idx) => (
-                <ChartSeriesItem
-                  key={idx}
-                  type="line"
-                  tooltip={{
-                    visible: true,
-                  }}
-                  data={item.data}
-                  name={item.name}
-                />
-              ))}
-            </ChartSeries>
-          </Chart>
-        </div>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-6">
-        <div className="k-card">
-          <Chart
-            style={{
-              height: 350,
-            }}
-          >
-            <ChartTitle text="Area Chart" />
-            <ChartLegend position="top" orientation="horizontal" />
-            <ChartCategoryAxis>
-              <ChartCategoryAxisItem categories={categories} startAngle={45} />
-            </ChartCategoryAxis>
-            <ChartSeries>
-              {areaData.map((item, idx) => (
-                <ChartSeriesItem
-                  key={idx}
-                  type="area"
-                  tooltip={{
-                    visible: true,
-                  }}
-                  data={item.data}
-                  name={item.name}
-                />
-              ))}
-            </ChartSeries>
-          </Chart>
-        </div>
-      </div>
-      <div className="col-6">
-        <div className="k-card">
-          <Chart
-            style={{
-              height: 350,
-            }}
-          >
-            <ChartTitle text="Pie Chart" />
-            <ChartLegend position="top" orientation="horizontal" />
-            <ChartSeries>
-              <ChartSeriesItem
-                type="pie"
-                overlay={{
-                  gradient: "sharpBevel",
+const App = () => {
+  const chunkArray = (array, size) => {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size));
+    }
+    return result;
+  };
+
+  // Split images into groups of 3
+  const imageChunks = chunkArray(data, 3);
+  console.log(imageChunks);
+
+  const handleClickEvent = (event) => {
+    const imgSrc = event.target.src;
+    alert(`Image clicked: ${imgSrc}`);
+  };
+
+  return (
+    <Container as={Row}>
+      {imageChunks.map((chunk, rowIndex) => (
+        <Row key={rowIndex} className="h-100">
+          {chunk.map((image, colIndex) => (
+            <Col xs={4} key={colIndex} className="g-2">
+              <img
+                onClick={(e) => handleClickEvent(e)}
+                id={"image" + rowIndex + colIndex || " "}
+                src={image["image-url"]}
+                alt={`image-${rowIndex}-${colIndex}`}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "contain",
                 }}
-                tooltip={{
-                  visible: true,
-                }}
-                data={pieData}
-                categoryField="name"
-                field="share"
               />
-            </ChartSeries>
-          </Chart>
-        </div>
-      </div>
-    </div>
-  </>
-);
-export default ChartContainer;
+            </Col>
+          ))}
+        </Row>
+      ))}
+    </Container>
+  );
+};
+export default App;
---
+++ src/components/ImageCarousel.jsx
@@ -0,0 +1,11 @@
+import Carousel from "react-bootstrap/Carousel";
+
+function UncontrolledExample({ image }) {
+  return (
+    <Carousel>
+      <Carousel.Item></Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default UncontrolledExample;
