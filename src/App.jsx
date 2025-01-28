e9ddb1ed47181f0bc0c29c089db71b3060daf729
KetanMediaamp <ketan.kedar@mediaamp.co.in>
2025-01-28 12:31:35 +0530

    working on react-image-gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,78 @@
 import "@progress/kendo-theme-default/dist/all.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Gallery } from "react-grid-gallery";
-import { Col, Container, Overlay, Row } from "react-bootstrap";
+// import { Gallery } from "react-grid-gallery";
+import { Container, Overlay } from "react-bootstrap";
 import ImageCarousel from "./components/ImageCarousel";
 import { useRef, useState } from "react";
+import StackGrid from "react-stack-grid";
 
 // console.log(data);
 const data = [
   {
-    src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+    thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
     original: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
     width: 320,
     height: 174,
     caption: "After Rain (Jeshu John - designerspics.com)",
   },
   {
-    src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+    thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
     original: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
     width: 320,
     height: 212,
     caption: "Boats (Jeshu John - designerspics.com)",
   },
   {
-    src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+    thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
     original: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
     width: 320,
     height: 212,
     caption: "Color Pencils (Jeshu John - designerspics.com)",
   },
   {
-    src: "https://c7.staticflickr.com/9/8546/28354329294_bb45ba31fa_b.jpg",
+    thumbnail: "https://c7.staticflickr.com/9/8546/28354329294_bb45ba31fa_b.jpg",
     original: "https://c7.staticflickr.com/9/8546/28354329294_bb45ba31fa_b.jpg",
     width: 320,
     height: 213,
     caption: "Red Apples with other Red Fruit (foodiesfeed.com)",
   },
   {
-    src: "https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_b.jpg",
+    thumbnail: "https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_b.jpg",
     original: "https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_b.jpg",
     width: 320,
     height: 183,
     caption: "37H (gratispgraphy.com)",
   },
   {
-    src: "https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_b.jpg",
+    thumbnail: "https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_b.jpg",
     original: "https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_b.jpg",
     width: 240,
     height: 320,
     caption: "8H (gratisography.com)",
   },
   {
-    src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
+    thumbnail: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
     original: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
     width: 320,
     height: 190,
     caption: "286H (gratisography.com)",
   },
   {
-    src: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
+    thumbnail: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
     original: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
     width: 320,
     height: 148,
     caption: "315H (gratisography.com)",
   },
   {
-    src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
+    thumbnail: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
     original: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
     width: 320,
     height: 213,
     caption: "201H (gratisography.com)",
   },
   {
-    src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
+    thumbnail: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
     original: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
     alt: "Big Ben - London",
     width: 248,
@@ -79,7 +80,7 @@ const data = [
     caption: "Big Ben (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c7.staticflickr.com/9/8785/28687743710_3580fcb5f0_b.jpg",
+    thumbnail: "https://c7.staticflickr.com/9/8785/28687743710_3580fcb5f0_b.jpg",
     original: "https://c7.staticflickr.com/9/8785/28687743710_3580fcb5f0_b.jpg",
     alt: "Red Zone - Paris",
     width: 320,
@@ -87,7 +88,7 @@ const data = [
     caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
+    thumbnail: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
     original: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
     alt: "Wood Glass",
     width: 313,
@@ -95,21 +96,21 @@ const data = [
     caption: "Wood Glass (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
+    thumbnail: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
     original: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
     width: 320,
     height: 213,
     caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c4.staticflickr.com/9/8562/28897228731_ff4447ef5f_b.jpg",
+    thumbnail: "https://c4.staticflickr.com/9/8562/28897228731_ff4447ef5f_b.jpg",
     original: "https://c4.staticflickr.com/9/8562/28897228731_ff4447ef5f_b.jpg",
     width: 320,
     height: 194,
     caption: "Old Barn (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c2.staticflickr.com/8/7577/28973580825_d8f541ba3f_b.jpg",
+    thumbnail: "https://c2.staticflickr.com/8/7577/28973580825_d8f541ba3f_b.jpg",
     original: "https://c2.staticflickr.com/8/7577/28973580825_d8f541ba3f_b.jpg",
     alt: "Cosmos Flower",
     width: 320,
@@ -117,63 +118,63 @@ const data = [
     caption: "Cosmos Flower Macro (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
+    thumbnail: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
     original: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
     width: 271,
     height: 320,
     caption: "Orange Macro (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c1.staticflickr.com/9/8330/28941240416_71d2a7af8e_b.jpg",
+    thumbnail: "https://c1.staticflickr.com/9/8330/28941240416_71d2a7af8e_b.jpg",
     original: "https://c1.staticflickr.com/9/8330/28941240416_71d2a7af8e_b.jpg",
     width: 320,
     height: 213,
     caption: "Surfer Sunset (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c1.staticflickr.com/9/8707/28868704912_cba5c6600e_b.jpg",
+    thumbnail: "https://c1.staticflickr.com/9/8707/28868704912_cba5c6600e_b.jpg",
     original: "https://c1.staticflickr.com/9/8707/28868704912_cba5c6600e_b.jpg",
     width: 320,
     height: 213,
     caption: "Man on BMX (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg",
+    thumbnail: "https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg",
     original: "https://c4.staticflickr.com/9/8578/28357117603_97a8233cf5_b.jpg",
     width: 320,
     height: 213,
     caption: "Ropeman - Thailand (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c4.staticflickr.com/8/7476/28973628875_069e938525_b.jpg",
+    thumbnail: "https://c4.staticflickr.com/8/7476/28973628875_069e938525_b.jpg",
     original: "https://c4.staticflickr.com/8/7476/28973628875_069e938525_b.jpg",
     width: 320,
     height: 213,
     caption: "Time to Think (Tom Eversley - isorepublic.com)",
   },
   {
-    src: "https://c6.staticflickr.com/9/8593/28357129133_f04c73bf1e_b.jpg",
+    thumbnail: "https://c6.staticflickr.com/9/8593/28357129133_f04c73bf1e_b.jpg",
     original: "https://c6.staticflickr.com/9/8593/28357129133_f04c73bf1e_b.jpg",
     width: 320,
     height: 179,
     caption: "Untitled (Jan Vasek - jeshoots.com)",
   },
   {
-    src: "https://c6.staticflickr.com/9/8893/28897116141_641b88e342_b.jpg",
+    thumbnail: "https://c6.staticflickr.com/9/8893/28897116141_641b88e342_b.jpg",
     original: "https://c6.staticflickr.com/9/8893/28897116141_641b88e342_b.jpg",
     width: 320,
     height: 215,
     caption: "Untitled (moveast.me)",
   },
   {
-    src: "https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_b.jpg",
+    thumbnail: "https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_b.jpg",
     original: "https://c1.staticflickr.com/9/8056/28354485944_148d6a5fc1_b.jpg",
     width: 257,
     height: 320,
     caption: "A photo by 贝莉儿 NG. (unsplash.com)",
   },
   {
-    src: "https://c7.staticflickr.com/9/8824/28868764222_19f3b30773_b.jpg",
+    thumbnail: "https://c7.staticflickr.com/9/8824/28868764222_19f3b30773_b.jpg",
     original: "https://c7.staticflickr.com/9/8824/28868764222_19f3b30773_b.jpg",
     width: 226,
     height: 320,
@@ -185,7 +186,7 @@ const App = () => {
   const handleClickEvent = (indx) => {
     setShow(!show);
     console.log(indx);
-    setIndex(indx)
+    setIndex(indx);
   };
   const [show, setShow] = useState(false);
 
@@ -193,13 +194,41 @@ const App = () => {
 
   return (
     <>
-      <Container>
         {show && (
           <Overlay target={target.current} show={show}>
             <ImageCarousel data={data} idx={index} />
           </Overlay>
         )}
-        <Row className="g-0 justify-content-center">
+      <Container>
+        <StackGrid
+          columnWidth="33.33%"
+          // gutterWidth={10} // Horizontal space between grid items
+          gutterHeight={10} // Vertical space between grid items
+        >
+          {data.map((item, imgIndex) => (
+            <div
+              key={imgIndex}
+              style={{
+                width: "100%",
+              }}
+              onClick={() => {
+                handleClickEvent(imgIndex);
+              }}
+            >
+              <img
+                src={item.thumbnail}
+                alt={`Item ${imgIndex}`}
+                style={{
+                  width: "100%",
+                  height: "auto",
+                  objectFit: "cover",
+                  borderRadius: "4px",
+                }}
+              />
+            </div>
+          ))}
+        </StackGrid>
+        {/* <Row className="g-0 justify-content-center">
           <Col xs={8}>
             <Gallery
               images={data}
@@ -209,7 +238,7 @@ const App = () => {
               enableImageSelection={false}
             />
           </Col>
-        </Row>
+        </Row> */}
       </Container>
     </>
   );
--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,17 +1,31 @@
-import Carousel from "react-bootstrap/Carousel";
+// import { useState } from "react";
+// import Carousel from "react-bootstrap/Carousel";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 function ImageCarousel({ data, idx }) {//eslint-disable-line
+  // const [index, setIndex] = useState(idx);
+
     return (
       <div
-        style={{
-          display: "flex",
-          transform: "translateY(-70%)",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
+        // style={{
+        //   display: "flex",
+        //   position: "absolute",
+        //   backgroundColor: "rgba(0, 0, 0, 0.5)",
+        //   top: 0,
+        //   height: "100vh",
+        //   width: "100vw",
+        //   justifyContent: "center",
+        //   alignItems: "center",
+        //   zIndex: 1000,
+        // }}
       >
-        <Carousel
-          activeIndex={idx}
+        {/* <Carousel
+          activeIndex={index}
+          onSelect={(selectedIndex) => {
+            setIndex(selectedIndex);
+          }}
+          interval={null}
           style={{ width: "80%", backgroundColor: "rgba(0, 0, 0, 0.5)" }}
         >
           {data.map((image,index ) => (//eslint-disable-line
@@ -31,7 +45,8 @@ function ImageCarousel({ data, idx }) {//eslint-disable-line
               </Carousel.Item>
             )
           )}
-        </Carousel>
+        </Carousel> */}
+        <ImageGallery items={data} s/>
       </div>
     );
 }
