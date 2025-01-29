d0fddf007baf2588bed793a86b505a137e08d1ad
KetanMediaamp <ketan.kedar@mediaamp.co.in>
2025-01-29 17:47:31 +0530

    major changes, removed needless test files and better fixed the routing of pages

--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import { Image } from "react-bootstrap";
-
-const Logo = ({ width = 50, height = 50 }) => {//eslint-disable-line
-  return (
-    <Image src="/images/icon.png" width={width} height={height} alt="icon" />
-  );
-};
-
-export default Logo;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,34 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@progress/kendo-theme-default/dist/all.css";
 import { createRoot } from "react-dom/client";
-import ChartContainer from "./App";
+import { PersistGate } from "redux-persist/integration/react";
+import Gallery from "./components/Gallery/Gallery";
+import Login from "./components/Login/Login";
+import { Provider } from "react-redux";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import { store, persistor } from "./components/store/config.js";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="login" element={<Login />} />
+      <Route path="gallery" element={<Gallery />} />
+    </Route>
+  )
+);
 
 const root = createRoot(document.querySelector("#root"));
-root.render(<ChartContainer />);
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={router} />
+    </PersistGate>
+  </Provider>
+);
